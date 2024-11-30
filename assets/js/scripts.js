9235ca2757456cb2208df45dde32aaea0b390690
td041 <trinhtrantrungduc@gmail.com>
2024-12-01 01:58:39 +0700

    Hoàn thiện form-filter và footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -154,16 +154,25 @@ function initJsToggle() {
     if (!target) {
       document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
     }
-    button.onclick = () => {
+    button.onclick = (e) => {
+      e.preventDefault();
       if (!$(target)) {
         return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
       }
       const isHidden = $(target).classList.contains("hide");
 
       requestAnimationFrame(() => {
-        $(target).classList.toggle("hide", !isHidden);
+        $(target).classList.toggle("hide", !isHidden);x
         $(target).classList.toggle("show", isHidden);
       });
+    };
+    document.onclick = function (e) {
+      if (!e.target.closest(target)) {
+        const isHidden = $(target).classList.contains("hide");
+        if (!isHidden) {
+          button.click();
+        }
+      }
     };
   });
 }
