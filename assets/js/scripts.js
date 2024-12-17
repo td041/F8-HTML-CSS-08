bcdf7ebb432bee6cc8f4a659dfae5726869ba748
td041 <trinhtrantrungduc@gmail.com>
2024-12-18 03:21:26 +0700

    Làm phần nội dung bài viết

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -188,3 +188,25 @@ window.addEventListener("template-loaded", () => {
     };
   });
 });
+// Tab-list
+window.addEventListener("template-loaded", () => {
+  const tabsSelector = "product-tab__item";
+  const contentsSelector = "product-tab__content";
+
+  const tabActive = `${tabsSelector}--active`;
+  const contentActive = `${contentsSelector}--active`;
+
+  const tabContainers = $$(".js-tabs");
+  tabContainers.forEach((tabContainer) => {
+      const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+      const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+      tabs.forEach((tab, index) => {
+          tab.onclick = () => {
+              tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+              tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+              tab.classList.add(tabActive);
+              contents[index].classList.add(contentActive);
+          };
+      });
+  });
+});
